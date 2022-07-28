bc07584d8b590db461981789b7bc6bdc008e954b
janpetrov <janpetrov@icloud.com>
2022-07-28 15:38:38 +0200

    .

---
+++ makepictures/py.py
@@ -0,0 +1,9 @@
+
+
+
+def print_line(self, remaining_height: int) -> int:
+    """
+    some function
+    @param: something
+    @returns: height of newly added line, None if no horizontal space remains
+    """
